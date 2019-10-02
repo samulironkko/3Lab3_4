040b4c823207a071c07027bce03a6b356ea5b11b
Samuli <samuli.ronkko@gmail.com>
2019-10-02 21:06:45 +0300

    kesken

--- app/src/main/java/com/example/a3lab3_4/MainActivity.java
+++ app/src/main/java/com/example/a3lab3_4/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -14,6 +15,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     LinearLayout layout2;
     LinearLayout layout3;
     LinearLayout layout4;
+
+    TextView textView1;
+    TextView textView2;
+    TextView textView3;
+    TextView textView4;
+
+    ArrayList<Tamagotchi> tamagotchis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         layout3 = findViewById(R.id.tama3);
         layout4 = findViewById(R.id.tama4);
 
+        textView1 = findViewById(R.id.text1);
+        textView2 = findViewById(R.id.text2);
+        textView3 = findViewById(R.id.text3);
+        textView4 = findViewById(R.id.text4);
+
+
 
         createFarm();
     }
@@ -33,8 +47,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
 
         switch (v.getId()) {
-            case R.id.tama1:
 
+            case R.id.tama1:
+                tamagotchis.get(0).setFood();
+                break;
+
+            case R.id.tama2:
+                tamagotchis.get(1).setFood();
+                break;
+
+            case R.id.tama3:
+                tamagotchis.get(2).setFood();
+                break;
+
+            case R.id.tama4:
+                tamagotchis.get(3).setFood();
                 break;
         }
 
@@ -43,11 +70,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void updateStatus(int foodLeft, int id) {
 
+        switch (id) {
+            case 0:
+                textView1.setText("Food Left: " + foodLeft);
+                break;
+
+            case 1:
+                textView2.setText("Food Left: " + foodLeft);
+                break;
+
+            case 2:
+                textView3.setText("Food Left: " + foodLeft);
+                break;
+
+            case 3:
+                textView4.setText("Food Left: " + foodLeft);
+                break;
+        }
+
     }
 
     public void createFarm() {
 
-        ArrayList<Tamagotchi> tamagotchis = new ArrayList<Tamagotchi>();
+        tamagotchis = new ArrayList<Tamagotchi>();
         Tamagotchi tamagotchi1 = new Tamagotchi(this, 0);
         Tamagotchi tamagotchi2 = new Tamagotchi(this, 1);
         Tamagotchi tamagotchi3 = new Tamagotchi(this, 2);
